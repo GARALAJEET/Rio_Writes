bde12b64cff565682b40595d85ab205dccc5567a
Jeet Garala <155524426+GARALAJEET@users.noreply.github.com>
2025-03-23 01:08:07 +0530

    init

--- Blogging-website/src/main/java/com/rio/Blogging/website/Controller/CategoryController.java
+++ Blogging-website/src/main/java/com/rio/Blogging/website/Controller/CategoryController.java
@@ -3,30 +3,36 @@ package com.rio.Blogging.website.Controller;
 import com.rio.Blogging.website.DTO.CategoryDto;
 import com.rio.Blogging.website.Modal.Category;
 import com.rio.Blogging.website.ServiceImp.CategorySeriveImp;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/api/category")
 public class CategoryController {
     @Autowired
     private CategorySeriveImp categorySeriveImp;
     @PostMapping("/createCategory")
-    public ResponseEntity<?>createCategory(@RequestBody CategoryDto categoryDto){
+    public ResponseEntity<?>createCategory( @Valid  @RequestBody CategoryDto categoryDto){
         return categorySeriveImp.createCategory(categoryDto);
     }
     @GetMapping("/getCategory/{id}")
-    public ResponseEntity<?>getCategory(Long id){
+    public ResponseEntity<?>getCategory( @PathVariable Long id){
         return categorySeriveImp.getCategory(id);
     }
     @GetMapping("/getAllCategories")
     public ResponseEntity<?>getAllCategories(){
         return categorySeriveImp.getAllCategories();
     }
-    @PutMapping
+    @PutMapping("/updateCategory/{id}")
     public ResponseEntity<?>updateCategory(@PathVariable Long id, @RequestBody CategoryDto categoryDto){
+        if(categoryDto==null){
+            return new ResponseEntity<>("Enter data to update", HttpStatus.BAD_REQUEST);
+        }
         return categorySeriveImp.updateCategory(id, categoryDto);
     }
     @DeleteMapping("/deleteCategory/{id}")
