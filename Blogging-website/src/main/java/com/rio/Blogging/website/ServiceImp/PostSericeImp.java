c6af1b671ebbf1b50d75247889a8831b016c82a0
Jeet Garala <155524426+GARALAJEET@users.noreply.github.com>
2025-04-05 11:46:17 +0530

    init

--- Blogging-website/src/main/java/com/rio/Blogging/website/ServiceImp/PostSericeImp.java
+++ Blogging-website/src/main/java/com/rio/Blogging/website/ServiceImp/PostSericeImp.java
@@ -13,6 +13,7 @@ import com.rio.Blogging.website.repo.userRepo;
 import com.rio.Blogging.website.service.postService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
